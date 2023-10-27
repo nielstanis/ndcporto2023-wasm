0260e48169f7447f6f030defb15403f848ee5fd3
nielstanis@live.nl <nielstanis@live.nl>
2023-10-27 14:07:09 +0200

    Finally content

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello NDC Porto 2023 from {RuntimeInformation.OSArchitecture}!");
+
+var file = System.IO.File.ReadAllText("/etc/hosts");
+Console.WriteLine(file);
---
+++ Extend/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Boa Tarde NDC Porto 2023!");
+
+Wasmtime.WasiConfiguration conf = new Wasmtime.WasiConfiguration()
+    .WithInheritedStandardError()
+    .WithInheritedStandardOutput()
+    .WithPreopenedDirectory("/etc", "/etc")
+    .WithPreopenedDirectory(".", ".")
+    .WithArg("CalledFromConsole")
+    .WithArgs(args); //passing args
+
+var engineConfig = new Wasmtime.Config();
+engineConfig.WithFuelConsumption(true);
+var engine = new Wasmtime.Engine(engineConfig);
+var linker = new Wasmtime.Linker(engine);
+linker.DefineWasi();
+var store = new Wasmtime.Store(engine);
+store.SetWasiConfiguration(conf);
+store.AddFuel(20000);
+
+string wasm = @"wasm/myrustmodule.wasm";
+var module = Wasmtime.Module.FromFile(engine, wasm);
+var inst = linker.Instantiate(store, module);
+var main = inst.GetFunction("main");
+
+if (main!=null)
+    main.Invoke(0,0);
+
+Console.WriteLine($"Consumed fuel: {store.GetConsumedFuel()}");
